98d24a4bd8e6c189c410190e511f59fdd3d173ef
Angelica Catalan <tummytum3@gmail.com>
2017-06-26 15:00:13 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name="Lineparser",
+      version="1.0",
+      description="",
+      author="Angelica Catalan",
+      py_modules=["lineparser", "settings"],
+      license="MIT"
+     )
