5d2530d5ef59f7039f748ab272c41ffb8fe1faeb
Tumthe3 <tumthe3@gmail.com>
2017-10-07 15:46:01 -0400

    Update username/email

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup
 setup(name="Pyliner",
       version="1.0",
       description="",
-      url="https://github.com/Tumthe3/Pyliner",
-      author="Tumthe3",
-      author_email="tumthe3@gmail.com",
+      url="https://github.com/LeicaSimile/Pyliner",
+      author="LeicaSimile",
+      author_email="leicasimile@gmail.com",
       packages=["pyliner"],
       license="MIT"
      )
