f8a026da5a21fb0c58934ad7e16fd3ffa7543cfc
Angelica Catalan <leicasimile@gmail.com>
2019-06-12 12:31:30 -0400

    Remove indent

--- pyliner/__init__.py
+++ pyliner/__init__.py
@@ -5,5 +5,4 @@ __author__ = "Angelica Catalan"
 __license__ = "MIT"
 __version__ = "0.1.2"
 
-from .pyliner import clean, parse_cases, parse_choices,
-    parse_optional, parse_all, regexp, titlecase
+from .pyliner import clean, parse_cases, parse_choices, parse_optional, parse_all, regexp, titlecase
